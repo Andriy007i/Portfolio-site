98fde04b087077a1c4ecb1ac33f8400f47ae9441
Andriy007i <andrii.zub@nure.ua>
2025-04-26 20:24:09 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const href = this.getAttribute('href').substring(1);
+        const target = document.getElementById(href);
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 80,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
